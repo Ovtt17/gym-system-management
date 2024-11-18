766cb1461829ebbdcff0376e7685222b292f6d2f
Ovett Mora <ovettmora17@gmail.com>
2024-11-17 20:20:13 -0600

    feat: update AddClientButton label to Spanish "Agregar Cliente"

--- src/components/client/AddClientButton.tsx
+++ src/components/client/AddClientButton.tsx
@@ -12,7 +12,7 @@ const AddClientButton: React.FC<AddClientButtonProps> = ({ redirectionUrl }) => 
       <button
         className="bg-blue-600 text-white px-4 py-2 rounded-md shadow hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300"
       >
-        Add Client
+        Agregar Cliente
       </button>
     </Link>
   );
